f00494ad842abaf904c392d74010ed6f7c2876c3
Artur <artur.fonseca.souza@gmail.com>
2024-11-07 15:57:13 -0300

    funcao bfs, encontra capital e adiciona aresta

---
+++ grafo.cpp
@@ -0,0 +1,45 @@
+#include "grafo.hpp"
+#include <queue>
+
+grafo::grafo(int vertices){
+
+}
+
+void grafo::AdicionaAresta(std::string origem,std::string destino){
+    grafo_[origem].push_back(destino);
+}
+
+std::string grafo::EncontraCapital(){
+    int max = 0;
+    std::string capital;
+    for(auto it=grafo_.begin(); it!=grafo_.end();it++){
+        int n = BFSadaptada(grafo_,it->first);
+        if(n>max){
+            max = n;
+            capital = it->first;
+        }
+    }
+    return capital;
+}
+
+int BFSadaptada(const std::map<std::string, std::vector<std::string>>& grafo, const std::string& inicial){
+    std::queue<std::string> fila;
+    std::map<std::string, bool> visitado;
+    int cont = 0;
+    visitado[inicial] = true;
+    fila.push(inicial);
+
+    while(!fila.empty()){
+        std::string atual = fila.front();
+        fila.pop();
+        cont ++;
+        for(const auto& vizinho : grafo.at(atual)){
+            if(visitado[vizinho]==false){
+                visitado[vizinho] == true;
+                fila.push(vizinho);
+            }
+
+        }
+    }
+    return cont;
+}
---
+++ grafo.hpp
@@ -0,0 +1,21 @@
+#ifndef GRAFO_HPP
+#define GRAFO_HPP
+#include <map>
+#include <string>
+#include <vector>
+
+class grafo{
+    private:
+        std::map<std::string, std::vector<std::string>> grafo_;
+    
+    public:
+        grafo(int vertices);
+        void AdicionaAresta(std::string origem, std::string destino);
+        std::string EncontraCapital();
+        ~grafo();
+
+};
+
+
+
+#endif
